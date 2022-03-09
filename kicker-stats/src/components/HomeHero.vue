98c77b4953d246ed96c25cacb11ff6a380a92ae0
Ben Schaser <benschaser@gmail.com>
2022-03-09 13:20:56 -0600

    added homehero

---
+++ kicker-stats/src/components/HomeHero.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container class='home-hero' fluid fill-height>
+    <v-row justify='center' align-content='center'>
+        <v-col>
+            <div class="text-h1 white--text font-weight-bold text-center">Welcome</div>
+            <div class="text-h3 white--text text-center">- TO THE -</div>
+            <div class="text-h1 white--text font-weight-bold text-center">Game Changer</div>
+            <!-- <div class="text-h1 yellow--text text--lighten-5 font-weight-bold text-center">Anytime, Anywhere</div> -->
+            <!-- <div class="text-h4 white--text text-center">Safe from Josh's pocket, items do not contain any lint. Note image is not to scale.</div> -->
+        </v-col>
+    </v-row>
+</v-container>
+</template>
+
+<script>
+export default ({
+    name: "HomeHero"
+})
+</script>
+
+<style scoped>
+.home-hero {
+    background-image: url("../assets/home-hero.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    text-shadow: 6px 6px 4px #002d2d;
+}
+</style>
--- kicker-stats/src/views/Home.vue
+++ kicker-stats/src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
     <span>
-        hello
+        <HomeHero></HomeHero>
+        <div class="paragraph">
+            <h2 class="text-h4 font-weight-bold text-center">A REVOLUTIONARY STATISTICS EXPERIENCE BUILT WITH REAL DATA</h2>
+            <p class="text-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus viverra accumsan in nisl nisi scelerisque eu. Sed ullamcorper morbi tincidunt ornare massa eget. Mauris commodo quis imperdiet massa tincidunt. Id velit ut tortor pretium. Egestas erat imperdiet sed euismod nisi. Proin libero nunc consequat interdum varius. Felis imperdiet proin fermentum leo vel orci. Viverra aliquet eget sit amet tellus cras adipiscing. Amet nisl purus in mollis. In egestas erat imperdiet sed euismod nisi porta lorem mollis.
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus viverra accumsan in nisl nisi scelerisque eu. Sed ullamcorper morbi tincidunt ornare massa eget. Mauris commodo quis imperdiet massa tincidunt. Id velit ut tortor pretium. Egestas erat imperdiet sed euismod nisi. Proin libero nunc consequat interdum varius. Felis imperdiet proin fermentum leo vel orci. Viverra aliquet eget sit amet tellus cras adipiscing. Amet nisl purus in mollis. In egestas erat imperdiet sed euismod nisi porta lorem mollis.
+            </p>
+        </div>
+        
     </span>
 </template>
 <script>
+import HomeHero from "@/components/HomeHero.vue";
 
 export default ({
     name: 'Home',
-    components: {  }
+    components: { HomeHero }
 })
 </script>
+<style scoped>
+.paragraph {
+    margin: 30px 60px;
+}
+.paragraph h2 {
+    margin-bottom: 10px;
+    color: #555555;
+}
+</style>
